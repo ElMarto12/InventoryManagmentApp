3e0fea3b4f930c028103db9c0b32b8860d6f1356
Martynas Danys <m10danys@gmail.com>
2025-10-28 17:27:32 +0200

    updated (reveal password input) functionality

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,21 @@
-﻿function revealPass(){
-    let passwordInput = document.getElementById("password");
-    passwordInput.getAttribute("type") === "password" ? passwordInput.setAttribute("type", "text") : passwordInput.setAttribute("type", "password");
+﻿function revealPass(inputElement, iconElement) {
+    const passwordInput = document.getElementById(inputElement);
+    const icon = document.getElementById(iconElement);
+    
+    if(passwordInput.getAttribute("type") === "password"){
+        passwordInput.setAttribute("type", "text");
+        icon.className = "bi bi-eye-slash-fill";
+    }
+    else{
+        passwordInput.setAttribute("type", "password");
+        icon.className = "bi bi-eye-fill";
+    }
 }
+
+document.querySelector("#pass-rev-button").addEventListener("click", function(){
+   revealPass("password", "pass-rev-icon"); 
+});
+
+document.querySelector("#repass-rev-button").addEventListener("click", function(){
+   revealPass("repeatpassword", "repass-rev-icon"); 
+});
